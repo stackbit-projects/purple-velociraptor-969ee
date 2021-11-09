6b179e882bda115b47dd324f9273a32f2dccce09
Stackbit Code Editor <projects@stackbit.com>
2021-11-09 07:47:04 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = withBundleAnalyzer({
     eslint: {
         // Allow production builds to successfully complete even if your project has ESLint errors.
         ignoreDuringBuilds: true
-    }
+    },
     webpack: (config, { webpack, dev }) => {
         // temporary: for local development -  edit package.json "@stackbit/components": "file:../stackbit-components" and uncomment the alias below.
         // config.resolve.alias['react'] = path.resolve('./node_modules/react');
